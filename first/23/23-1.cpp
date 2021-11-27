3be37e5fc3b7d3c9d5b13abea92afb43bd484365
tmj <3536115985@qq.com>
2021-11-27 12:31:59 +0800

    master

---
+++ first/23/23-1.cpp
@@ -0,0 +1,15 @@
+#include<winsock.h>
+#include<iostream>
+#pragma comment(lib,"WS2_32.lib")
+using namespace std;
+
+int main()
+{
+    WSADATA data;
+    WORD w = MAKEWORD(2, 0);
+    int result=WSAStartup(w, &data);
+    SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+    /*  */
+    system("pause");
+    return 0;
+}
